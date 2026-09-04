7c6d6695b209ebc5f8a21cc46a923b127d0be7c3
pragatipy <pragati.yadav.cs27@gmail.com>
2026-09-04 20:33:27 +0530

    Triangular no code

---
+++ TriangularNo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TriangularNo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<n;i++){
+            sum+=i;
+            if(sum==n){
+                System.out.println(n+" is a triangular number.");
+                return;
+            }
+            if(sum>n){
+                break;
+            }
+        }
+        System.out.println(n+" is not a triangular number.");
+    }
+    
+}
